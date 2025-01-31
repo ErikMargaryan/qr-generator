10038c8c74f8f737fb36a6f924bf0f337d06301e
erik.margaryan <erik.margaryan@arca.am>
2025-01-31 17:15:08 +0400

    did corrections

--- src/main/java/com/riccardo/qr/LtEncoder.java
+++ src/main/java/com/riccardo/qr/LtEncoder.java
@@ -45,38 +45,41 @@ public class LtEncoder {
     // "Ideal Soliton" distribution
     private int idealSolitonDegree(int k) {
         double[] prob = new double[k];
-        prob[0] = 1.0/k; // P=1/k for deg=1
-        for (int d=2; d<=k; d++) {
-            prob[d-1] = 1.0/(d*(d-1));
+        prob[0] = 1.0 / k; // P=1/k for deg=1
+        for (int d = 2; d <= k; d++) {
+            prob[d - 1] = 1.0 / (d * (d - 1));
         }
-        for(int i=1; i<k; i++){
-            prob[i]+=prob[i-1];
+        for (int i = 1; i < k; i++) {
+            prob[i] += prob[i - 1];
         }
         double r = random.nextDouble();
-        for(int i=0; i<k; i++){
-            if(r<=prob[i]) return i+1;
+        for (int i = 0; i < k; i++) {
+            if (r <= prob[i]) return i + 1;
         }
         return k;
     }
 
     private byte[] xorBlocks(int[] idxs) {
         byte[] result = new byte[sliceSize];
-        for(int idx : idxs){
+        for (int idx : idxs) {
             byte[] b = blocks.get(idx);
-            for(int i=0; i<result.length; i++){
-                byte val = (i<b.length)? b[i] : 0;
-                result[i]^=val;
+            for (int i = 0; i < result.length; i++) {
+                byte val = (i < b.length) ? b[i] : 0;
+                result[i] ^= val;
             }
         }
         return result;
     }
 
     class FountainIterator implements Iterator<EncodedBlock> {
-        public boolean hasNext(){return true;}
-        public EncodedBlock next(){
+        public boolean hasNext() {
+            return true;
+        }
+
+        public EncodedBlock next() {
             int deg = idealSolitonDegree(k);
             Set<Integer> chosen = new HashSet<>();
-            while(chosen.size()<deg){
+            while (chosen.size() < deg) {
                 chosen.add(random.nextInt(k));
             }
             int[] arr = chosen.stream().mapToInt(Integer::intValue).sorted().toArray();
